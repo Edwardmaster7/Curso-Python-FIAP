dc765728f08ac02934e76db08b7332f0456d4f8c
Eduardo Batista <63757526+Edwardmaster7@users.noreply.github.com>
2025-10-13 09:18:22 -0300

    first commit

---
+++ sandbox/atv1.py
@@ -0,0 +1,15 @@
+def main():
+    print("Bem-vindo a atividade 1!\nInforme os dados do usuário abaixo:")
+    id = int
+    nome = str
+    email = str
+    ativo = bool
+    tags = []
+    
+    id = input("Insira o ID:")
+    if id not int:
+        id = input("Valor inválido! Insira um valor de id válido:")
+
+
+if __name__ == "__main__":
+    main()
